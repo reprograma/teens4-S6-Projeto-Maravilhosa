843307e063aa2eef2116e65c1bb7e2478dfb0957
lianaalice <lyannalisse@gmail.com>
2021-12-22 13:38:55 -0300

    add alunas projetos

---
+++ emily-marques/script.js
@@ -0,0 +1,14 @@
+const inputNome = document.querySelector("#input-nome");
+const inputRecado = document.querySelector("#input-recado");
+const formulario = document.querySelector("#formulario");
+
+
+function enviarFormulario (evento) {
+    evento.preventDefault();
+
+    console.log("cliquei onde? aqui");
+}
+
+
+//quando o formulario for enviado eu quero que funcione a função enviarFormulario 
+formulario.addEventListener("submit", enviarFormulario)
